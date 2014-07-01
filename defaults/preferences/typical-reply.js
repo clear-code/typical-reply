7e916a2fc4672f4229dc8714be20d0885b74ada9
YUKI Hiroshi <shimoda@clear-code.com>
2014-07-01 11:23:46 +0900

    Add localized messages

---
+++ defaults/preferences/typical-reply.js
@@ -0,0 +1,50 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+pref("extensions.typical-reply@clear-code.com.replying.type", "");
+pref("extensions.typical-reply@clear-code.com.replying.quote", false);
+
+pref("extensions.typical-reply@clear-code.com.buttons", "accept,reject,like");
+
+pref("extensions.typical-reply@clear-code.com.reply.accept.label", "chrome://typical-reply/locale/typical-reply.properties");
+pref("extensions.typical-reply@clear-code.com.reply.accept.accesskey", "chrome://typical-reply/locale/typical-reply.properties");
+pref("extensions.typical-reply@clear-code.com.reply.accept.subjectPrefix", "chrome://typical-reply/locale/typical-reply.properties");
+pref("extensions.typical-reply@clear-code.com.reply.accept.subject", "chrome://typical-reply/locale/typical-reply.properties");
+pref("extensions.typical-reply@clear-code.com.reply.accept.body", "chrome://typical-reply/locale/typical-reply.properties");
+pref("extensions.typical-reply@clear-code.com.reply.accept.recipients", "sender");
+pref("extensions.typical-reply@clear-code.com.reply.accept.alwaysQuote", false);
+pref("extensions.typical-reply@clear-code.com.reply.accept.priority", "");
+pref("extensions.typical-reply@clear-code.com.reply.accept.separate", true);
+pref("extensions.typical-reply@clear-code.com.reply.accept.searchFolder", false);
+pref("extensions.typical-reply@clear-code.com.reply.accept.icon", "chrome://typical-reply/locale/typical-reply.properties");
+pref("extensions.typical-reply@clear-code.com.reply.accept.bodyImage", "chrome://typical-reply/locale/typical-reply.properties");
+
+pref("extensions.typical-reply@clear-code.com.reply.reject.label", "chrome://typical-reply/locale/typical-reply.properties");
+pref("extensions.typical-reply@clear-code.com.reply.reject.accesskey", "chrome://typical-reply/locale/typical-reply.properties");
+pref("extensions.typical-reply@clear-code.com.reply.reject.subjectPrefix", "chrome://typical-reply/locale/typical-reply.properties");
+pref("extensions.typical-reply@clear-code.com.reply.reject.subject", "chrome://typical-reply/locale/typical-reply.properties");
+pref("extensions.typical-reply@clear-code.com.reply.reject.body", "chrome://typical-reply/locale/typical-reply.properties");
+pref("extensions.typical-reply@clear-code.com.reply.reject.recipients", "sender");
+pref("extensions.typical-reply@clear-code.com.reply.reject.alwaysQuote", false);
+pref("extensions.typical-reply@clear-code.com.reply.reject.priority", "");
+pref("extensions.typical-reply@clear-code.com.reply.reject.separate", false);
+pref("extensions.typical-reply@clear-code.com.reply.reject.searchFolder", false);
+pref("extensions.typical-reply@clear-code.com.reply.reject.icon", "chrome://typical-reply/locale/typical-reply.properties");
+pref("extensions.typical-reply@clear-code.com.reply.reject.bodyImage", "chrome://typical-reply/locale/typical-reply.properties");
+
+pref("extensions.typical-reply@clear-code.com.reply.like.label", "chrome://typical-reply/locale/typical-reply.properties");
+pref("extensions.typical-reply@clear-code.com.reply.like.accesskey", "chrome://typical-reply/locale/typical-reply.properties");
+pref("extensions.typical-reply@clear-code.com.reply.like.subjectPrefix", "chrome://typical-reply/locale/typical-reply.properties");
+pref("extensions.typical-reply@clear-code.com.reply.like.subject", "chrome://typical-reply/locale/typical-reply.properties");
+pref("extensions.typical-reply@clear-code.com.reply.like.body", "chrome://typical-reply/locale/typical-reply.properties");
+pref("extensions.typical-reply@clear-code.com.reply.like.recipients", "blank");
+pref("extensions.typical-reply@clear-code.com.reply.like.alwaysQuote", true);
+pref("extensions.typical-reply@clear-code.com.reply.like.priority", "");
+pref("extensions.typical-reply@clear-code.com.reply.like.separate", false);
+pref("extensions.typical-reply@clear-code.com.reply.like.searchFolder", true);
+pref("extensions.typical-reply@clear-code.com.reply.like.icon", "chrome://typical-reply/locale/typical-reply.properties");
+pref("extensions.typical-reply@clear-code.com.reply.like.bodyImage", "chrome://typical-reply/locale/typical-reply.properties");
+
+pref("extensions.typical-reply@clear-code.com.label.quote.before", "chrome://typical-reply/locale/typical-reply.properties");
+pref("extensions.typical-reply@clear-code.com.label.quote.after", "chrome://typical-reply/locale/typical-reply.properties");
