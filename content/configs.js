46a2f980be2d90ac579dc4036f3696ffe1364624
YUKI "Piro" Hiroshi <piro.outsider.reflex@gmail.com>
2020-08-05 17:36:48 +0900

    Implement dialog (WIP)

--- content/configs.js
+++ content/configs.js
@@ -2,19 +2,92 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+const { Services } = Components.utils.import('resource://gre/modules/Services.jsm', {});
 const { prefs } = Components.utils.import('resource://typical-reply-modules/prefs.js', {});
 const { TypicalReply } = Components.utils.import('resource://typical-reply-modules/TypicalReply.jsm', {});
 
+const bundle = Services.strings.createBundle('chrome://typical-reply/locale/typical-reply.properties');
+document.title = bundle.GetStringFromName('configs_title');
+
+const DEFINITION_FIELDS = `
+  label
+  accesskey
+  subjectPrefix
+  subject
+  body
+  bodyImage
+  recipients
+  quoteType
+  forwardType
+  priority
+  separate
+  searchFolder
+  searchTargets
+  allowedDomains
+  autoSend
+  icon
+`.trim().split(/\s+/);
+
 let mDOMUpdater;
+
 let mDefinitions;
+let mAddButton;
+let mEditDialogContainer;
+let mEditDialogContents;
+let mEditSaveButton;
+let mEditCancelButton;
 
 window.addEventListener('DOMContentLoaded', async () => {
   const { DOMUpdater } = await import('resource://typical-reply-modules/extlib/dom-updater.js');
   mDOMUpdater = DOMUpdater;
 
-  mDefinitions = document.getElementById('definitions');
+  mDefinitions = document.querySelector('#definitions');
+  mDefinitions.addEventListener('dblclick', event => {
+    const item = event.target.closest('li');
+    if (item)
+      shoeEditDialogFor(item.id);
+  });
   updateDefinitionsList();
+
+  mAddButton = document.querySelector('#add-item-button');
+  mAddButton.textContent = bundle.GetStringFromName('configs_addItem_label');
+  setButtonAction(mAddButton, event => {
+  });
+
+  mEditDialogContainer = document.querySelector('#edit-dialog-container');
+  for (const name of DEFINITION_FIELDS) {
+    const field = mEditDialogContainer.querySelector(`[data-field="${name}"]`);
+    const labelText = field.closest('label, div').querySelector('.label-text');
+    const key = `configs_editField${name.charAt(0).toUpperCase()}${name.substring(1)}_label`;
+    labelText.textContent = bundle.GetStringFromName(key);
+  }
+
+  mEditSaveButton = document.querySelector('#edit-save-button');
+  mEditSaveButton.textContent = bundle.GetStringFromName('configs_editSave_label');
+  setButtonAction(mEditSaveButton, event => {
+    hideEditDialog();
+  });
+
+  mEditCancelButton = document.querySelector('#edit-cancel-button');
+  mEditCancelButton.textContent = bundle.GetStringFromName('configs_editCancel_label');
+  setButtonAction(mEditCancelButton, event => {
+    hideEditDialog();
+  });
 }, { once: true });
+
+function setButtonAction(button, action) {
+  button.addEventListener('click', event => {
+    event.stopPropagation();
+    if (event.button == 0)
+      action(event);
+  });
+  button.addEventListener('keydown', event => {
+    event.stopPropagation();
+    if (event.key == 'Enter')
+      action(event);
+  });
+}
+
 
 function updateDefinitionsList() {
   const items = TypicalReply.definitions.map(definitionToListItem).join('');
@@ -24,23 +97,41 @@ function updateDefinitionsList() {
     <ul id="definitions">${items}</ul>
   `.trim());
   range.detach();
-  try {
-    mDOMUpdater.update(mDefinitions, after);
-  }
-  catch(error) {
-    console.error(error);
-  }
+  mDOMUpdater.update(mDefinitions, after);
 }
 
 function definitionToListItem(definition) {
   return `
-    <li id=${JSON.stringify(sanitizeForXMLText(definition.type))}>
-      <label>${sanitizeForXMLText(definition.label)}</label>
+    <li id=${JSON.stringify(sanitizeForHTMLText(definition.type))}>
+      <label>${sanitizeForHTMLText(definition.label)}</label>
       <button class="delete"></button>
     </li>
   `.trim();
 }
 
-function sanitizeForXMLText(text) {
+
+function shoeEditDialogFor(type) {
+  const definition = TypicalReply.getDefinition(type);
+  for (const name of DEFINITION_FIELDS) {
+    const field = mEditDialogContainer.querySelector(`[data-field="${name}"]`);
+    switch (field.type) {
+      case 'checkbox':
+        field.checked = !!definition[name];
+        break;
+
+      default:
+        field.value = definition[name];
+        break;
+    }
+  }
+  mEditDialogContainer.classList.add('shown');
+}
+
+function hideEditDialog() {
+    mEditDialogContainer.classList.remove('shown');
+}
+
+
+function sanitizeForHTMLText(text) {
   return text.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
 }
